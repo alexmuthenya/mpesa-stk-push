128fe829ab0d440f916a3b80227da40dff98e8f4
Alex <alexmuthenya100@gmail.com>
2026-01-26 18:47:29 +0300

    feat: add getTimestamp utility to generate formatted YYYYMMDDHHMMSS timestamps

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
 
 
-
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 5000
-
-
-app.listen(PORT, console.log(`Server running on port ${PORT}`)
-
-)
+app.listen(PORT, () => 
+  console.log(`Server running on port ${PORT}`));
