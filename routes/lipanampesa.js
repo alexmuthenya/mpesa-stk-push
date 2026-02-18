9c52d9fe7fd3850fd767801932d57076f2972625
Alex <alexmuthenya100@gmail.com>
2026-02-18 09:30:46 +0300

    feat(routes): add STK Push callback route

--- routes/lipanampesa.js
+++ routes/lipanampesa.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { initiateSTKPush } from '../controller/initiatePayment.js';
+import { initiateSTKPush, stkPushCallback } from '../controller/initiatePayment.js';
 import { accessToken } from '../middleware/generateAccessToken.js';
 const router = express.Router()
 
 
 router.post('/stkPush', accessToken, initiateSTKPush)
+router.post('/stkPushCallback/:Order_ID', stkPushCallback)
 
 export default router
