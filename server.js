df60cedff5805fb10f96db46c8202389070a7fea
Alex <alexmuthenya100@gmail.com>
2026-02-18 09:32:39 +0300

    chore(server.js): register STK Push routes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import router from "./routes/lipanampesa.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use('/api', router)
 
 
 app.listen(PORT, () => 
